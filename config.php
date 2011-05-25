1b91f4333e4be2971179800546364c40a3b702c0
Neil Duffy <neiladuffy@gmail.com>
2011-05-25 10:53:00 -0500

    second commit, of the rest

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'dLHHtEFC093N6uCWT4HdAFlAWm1iLKq_P7vX4VMppwDItPAkWrBvGBQMghYC3jsr'); /* aka your application's "API Key" */
+define('CONSUMER_SECRET', 'ybN6skI0spPEqmOt3ys9tS56h0UfpfTt2fNDiusPMS-nj86yNaA3ilgnEx3RIs0V'); /* aka your application's "Secret Key" */
+define('OAUTH_CALLBACK', 'http://neilbox.local/~neild/redonk/li-testing/franken/callback.php');
